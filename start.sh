2bf4dd821de6c597df9e5f098f2228769a6b2463
alejandrofontanil <alejandrofontanil@gmail.com>
2025-09-27 11:44:28 +0000

    feewf

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -o xtrace
 echo "--- Iniciando script de arranque ---"
 
 echo "-> Ejecutando migración de la base de datos..."
-flask db upgrade
+python -m flask db upgrade
 
 echo "-> Migración finalizada. Arrancando el servidor Gunicorn..."
 gunicorn "mi_app:create_app()"
