f5667e71da2a4ddc90ff45574898d51c4862542f
alejandrofontanil <alejandrofontanil@gmail.com>
2025-10-02 20:13:54 +0000

    sdsd

---
+++ migrations/versions/8b99f223aadd_add_rag_tokens_restantes_to_usuario_.py
@@ -0,0 +1,24 @@
+"""Add rag_tokens_restantes to Usuario model
+
+Revision ID: 8b99f223aadd
+Revises: 2cb258bb54b5
+Create Date: 2025-10-02 20:13:43.000832
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8b99f223aadd'
+down_revision = '2cb258bb54b5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
